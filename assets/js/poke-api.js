05394ea053d70f2ed3f1651f7ca2187e93a8a3be
alexgnunes <108927785+alexgnunes@users.noreply.github.com>
2024-01-08 07:01:55 -0200

    conversao odelos

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,14 +1,20 @@
 const pokeApi = {}
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
-    // const offset = 0;
-    // const limit = 10;
-    const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+pokeApi.getPokemonDetail = (pokemon) => {
+  return fetch(pokemon.url).then((response) => response.json());
+};
 
-    return fetch(url)
+pokeApi.getPokemons = (offset = 0, limit = 10) => {
+  // const offset = 0;
+  // const limit = 10;
+  const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
+
+  return fetch(url)
     .then((response) => response.json())
-    .then((jasonBody) =>jasonBody.results)
-    .then((pokemons) => )
-    .catch((error) => console.error(error));
+    .then((jasonBody) => jasonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails)
+  // .catch((error) => console.error(error));
 }
 
