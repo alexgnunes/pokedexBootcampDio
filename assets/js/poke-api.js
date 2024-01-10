d3aad51d7857de68d91f212b77ee977009611f2c
alexgnunes <alexgaribalde_tb@hotmail.com>
2024-01-10 20:14:44 -0300

    css por typo

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,12 +1,22 @@
 const pokeApi = {}
 
+function convertPokeApiToPokemon(pokemonDetail){
+    const pokemon = new Pokemon();
+    pokemon.number = pokemonDetail.order;
+    pokemon.name = pokemonDetail.name;
+    pokemon.types = pokemonDetail.types.map((typeSlot) => typeSlot.type.name);
+    pokemon.type = pokemon.types[0];
+    pokemon.photo =  pokemonDetail.sprites.front_default;
+    return pokemon;
+}
+
 pokeApi.getPokemonDetail = (pokemon) => {
-  return fetch(pokemon.url).then((response) => response.json());
+  return fetch(pokemon.url)
+  .then((response) => response.json())
+  .then(convertPokeApiToPokemon);
 };
 
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
-  // const offset = 0;
-  // const limit = 10;
+pokeApi.getPokemons = (offset = 0, limit = 150) => {
   const url = `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`;
 
   return fetch(url)
@@ -15,6 +25,5 @@ pokeApi.getPokemons = (offset = 0, limit = 10) => {
     .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
     .then((detailRequests) => Promise.all(detailRequests))
     .then((pokemonsDetails) => pokemonsDetails)
-  // .catch((error) => console.error(error));
 }
 
